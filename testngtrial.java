bf23e34ce11404689be7d914403b603fcd1e2d0a
Swegit1 <90357697+Swegit1@users.noreply.github.com>
2021-10-08 19:33:24 +0200

    testng failure
    driver not invoked

---
+++ testngtrial.java
@@ -0,0 +1,58 @@
+package com.practice.com.practice.maven.eclipse;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class testngtrial {
+public static WebDriver driver;
+	@BeforeSuite
+	public void initiate() {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+	}
+	@Test
+	public void search() {
+		driver.manage().deleteAllCookies();
+		driver.manage().window().maximize();
+		driver.get("https://www.twoplugs.com/");
+		driver.findElement(By.xpath("//a[contains(text(),'Live Posting')]")).click();
+		WebElement drpdwn=driver.findElement(By.name("category_id"));
+		Select drpselect=new Select(drpdwn);
+		List<WebElement> options=drpselect.getOptions();
+		ArrayList org=new ArrayList();
+		ArrayList temp=new ArrayList();
+		for (WebElement option:options)
+		{
+			org.add(option.getText());
+			temp.add(option.getText());
+			
+		}
+		System.out.println("Original list "+org);
+		System.out.println("Temp list "+temp);
+		Collections.sort(temp);
+		System.out.println("aftersorting"+temp);
+		
+		
+		
+	}
+		
+	@AfterSuite
+	public void end() {
+		driver.quit();
+		
+		
+	}
+
+}
